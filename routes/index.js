01c7c1bda66c9cdef2c1fd93886c5ade3ed63920
linken0523 <kenlin@student.unimelb.edu.au>
2020-09-01 19:55:02 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/campus%20life', function(req, res, next) {
+  res.render('campuslife', { title: 'Campus Life' });
+});
+router.get('/Skill', function(req, res, next) {
+  res.render('skill', { title: 'Campus Life' });
 });
 
 module.exports = router;
