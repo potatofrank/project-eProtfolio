3ac6a5ade4ff407666a2f6fd90c8bb1e7f415ddd
potatofrank <62005928+potatofrank@users.noreply.github.com>
2020-09-08 15:22:47 +1000

    update
    sign-up API
    log-in API
    sign-up encryption
    log-in encyption

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('home', { title: 'Express' });
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about', function(req, res) {
   res.render('about');
 });
 
-router.get('/campus_life', function(req, res, next) {
+router.get('/campus_life', function(req, res) {
   res.render('campus life');
 });
 
-router.get('/skills', function(req, res, next) {
+router.get('/skills', function(req, res) {
   res.render('skills');
 });
 
-router.get('/projects', function(req, res, next) {
+router.get('/projects', function(req, res) {
   res.render('projects');
 });
 
-router.get('/music', function(req, res, next) {
+router.get('/music', function(req, res) {
   res.render('music');
 });
 
