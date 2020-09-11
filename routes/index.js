10c37dccc924326da5e9a53e71b86db2cf559e32
yannanli1 <64351795+yannanli1@users.noreply.github.com>
2020-09-11 13:39:05 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+router.get('/campus_life', function(req, res, next) {
+  res.render('campus life');
+});
+
+router.get('/skills', function(req, res, next) {
+  res.render('skills');
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects');
+});
+
+router.get('/music', function(req, res, next) {
+  res.render('music');
 });
 
 module.exports = router;
