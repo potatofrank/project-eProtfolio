91d3bb56f1c00d423505ff6e479eff6d409daf64
potatofrank <62005928+potatofrank@users.noreply.github.com>
2020-09-12 13:39:21 +1000

    merge
    Combined work patch update

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,6 @@ router.get('/', function(req, res) {
   res.render('home', { title: 'Express' });
 });
 
-router.get('/about', function(req, res) {
-  res.render('about');
-});
-
-router.get('/campus_life', function(req, res) {
-  res.render('campus life');
-});
-
-router.get('/skills', function(req, res) {
-  res.render('skills');
-});
-
 router.get('/projects', function(req, res) {
   res.render('projects');
 });
